9dae5b5a60c9ac57e0e4b63b4f7a0d7726d6caeb
DrDhemm <idyusril@gmail.com>
2020-03-18 04:08:06 +0700

    clean console

--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -9,7 +9,6 @@ class Home extends Component {
     componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/posts')
             .then(res => {
-                console.log(res);
                 this.setState({
                     posts: res.data.slice(0, 10)
                 })
